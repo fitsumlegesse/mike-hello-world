c8c1e20ff021e8bf69619622a0acf7b0bc239d86
Fitsum Legesse <55632280+fitsumlegesse@users.noreply.github.com>
2022-05-21 18:31:46 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World from Mike 
         </a>
       </header>
     </div>
